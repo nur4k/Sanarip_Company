71b6ac857f77476927d530d6faccd4e69308e071
user <panpanzov.nur@bk.ru>
2023-07-28 16:29:37 +0600

    add class in views.py

--- geometry_app/views.py
+++ geometry_app/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from geometry_app.models import Region, District, Canton, Contour
+from geometry_app.serializers import RegionSerialzier, DistrictSerialzier, CantonSerialzier, ContourSerialzier
+
+
+class RegionView(generics.ListCreateAPIView):
+    queryset = Region.objects.all()
+    serializer_class = RegionSerialzier
+
+
+class DistrictView(generics.ListCreateAPIView):
+    queryset = District.objects.all()
+    serializer_class = DistrictSerialzier
+
+
+class CantonView(generics.ListCreateAPIView):
+    queryset = Canton.objects.all()
+    serializer_class = CantonSerialzier
+
+
+class ContourView(generics.ListCreateAPIView):
+    queryset = Contour.objects.all()
+    serializer_class = ContourSerialzier
